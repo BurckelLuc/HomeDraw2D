eb45244b4b20b1f46aa68e60fa86d5b94c2cad40
Caleb Mukaya <caleb.mukaya23@gmail.com>
2024-05-13 00:55:52 +0200

    [COMMANDS] working erase tool

--- src/app/core/tools/eraseTool.ts
+++ src/app/core/tools/eraseTool.ts
@@ -15,61 +15,12 @@ export class EraseTool extends BasicTool {
 
 	leftClick(point: Point, shapeService: ShapesService, clickedOnShape: Option<Shape>): Option<ICommand> {
 
-		if (this.currentPoint) {
 			if (clickedOnShape.isSome()) {
-				return this.onClickOnShape(point, clickedOnShape.unwrap(), shapeService);
+				return Option.Some(new RemoveShapeCommand(clickedOnShape.unwrap(), shapeService));
 			}
-
-			this.currentPoint = this.hoverPoint ?? point;
-
-
-		}else {
-			this.currentPoint = this.hoverPoint ?? point;
-			if (clickedOnShape.isSome() && clickedOnShape.unwrap() instanceof Wall) {
-				let shape = clickedOnShape.unwrap() as Wall
-				clickedOnShape.ifSome((shape) => shapeService.setCurrentShape(shape))
-
-			}
-		}
-
 
 		return Option.None();
 	}
-
-	onClickOnShape(
-		point: Point,
-		shape: Shape,
-		shapeService: ShapesService,
-	): Option<ICommand> {
-		if (!(shape instanceof Wall)) return Option.None();
-
-		let currentShape = shapeService.getCurrentShape();
-		if (!currentShape) {
-			this.currentPoint = point;
-
-			return Option.Some(
-				new RemoveShapeCommand(shape, shapeService),
-			);
-		}
-
-		// todo : remove shape from extendedShapeCommand
-		// todo: delete it
-
-		if (currentShape.id == shape.id) {
-			// Clicked on Self
-			let commands: ICommand[] = []
-
-			commands.push(new RemoveShapeCommand(shape, shapeService));
-			return Option.Some(new CombinedCommand(commands, shapeService));
-		}else {
-			let commands: ICommand[] = []
-			this.currentPoint = point;
-
-			commands.push(new RemoveShapeCommand(shapeService.getShapebyId(currentShape.id), shapeService));
-			return Option.Some(new CombinedCommand(commands, shapeService));
-		}
-	}
-
 	override rightClick(shapeService: ShapesService): Option<ICommand> {
 		return Option.None();
 	}
